ef1f6c119346229ed1e26a79799dda88fe242301
shivayasala <shivakumar.630485@gmail.com>
2024-11-18 17:27:07 +0530

    Fix MIME type issues and update paths

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set base URL for correct path resolution
+  base: '/portfolio-project1/',  // Ensure the base path is set correctly
 });
