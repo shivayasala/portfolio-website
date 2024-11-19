8155a0e206119c687472c2454b5fb239a6b5031a
shivayasala <shivakumar.630485@gmail.com>
2024-11-19 13:23:48 +0530

    Fix App component import and JSX handling

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './src/App'
+import App from './src/App';
 
 // Create the root element
 const rootElement = document.getElementById('root');
