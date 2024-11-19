7a07a9dce743d2ad8d1aeed41b9211e81bab85c4
shivayasala <shivakumar.630485@gmail.com>
2024-11-19 12:12:55 +0530

    Fix MIME type issues by renaming .jsx to .js and updating Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
   server: {
     mimeTypes: {
       'application/javascript': ['js', 'jsx']
