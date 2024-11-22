83dc293e150dbc5ee65325dbbf29b250ddb557ae
shivayasala <shivakumar.630485@gmail.com>
2024-11-22 10:05:27 +0530

    Added new features and fixed bugs

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './src/App3'; // After renaming App3.jsx to App3.js
+import App from './src/App3.jsx'; // Updated to App3.jsx
 
 // Create the root element
 const rootElement = document.getElementById('root');
